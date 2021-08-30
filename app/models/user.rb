471a89d6838efa3a143f925dbf522053f799f390
Carlos <carlosmontesinos1990@gmail.com>
2021-08-30 18:15:16 +0200

    Created assotiation and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  has_many :customer_bookings, source: :bookings
+  has_many :vinyls
+  has_many :owner_bookings, through: :vinyls, source: bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
