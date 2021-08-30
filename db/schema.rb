5337b6508266e94e0720ed93d40ffdd6a6f7ae15
Carlos <carlosmontesinos1990@gmail.com>
2021-08-30 16:31:38 +0200

    created two models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_140319) do
+ActiveRecord::Schema.define(version: 2021_08_30_142945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "vinyl_id", null: false
+    t.bigint "user_id", null: false
+    t.integer "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+    t.index ["vinyl_id"], name: "index_bookings_on_vinyl_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +39,20 @@ ActiveRecord::Schema.define(version: 2021_08_30_140319) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "vinyls", force: :cascade do |t|
+    t.string "album_name"
+    t.string "artist"
+    t.string "description"
+    t.string "condition"
+    t.string "genre"
+    t.float "daily_rate"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_vinyls_on_user_id"
+  end
+
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "vinyls"
+  add_foreign_key "vinyls", "users"
 end
