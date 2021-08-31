f668d3f5bd4ee2a9b6d85ed36ea7e8fc11c7fba2
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-08-31 17:56:48 +0200

    Avatar image and default avatar image set

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def avatar_url
+    if avatar.attached?
+      avatar.service_url
+    else
+      "https://simulacionymedicina.es/wp-content/uploads/2015/11/default-avatar-300x300-1.jpg"
+    end
+  end
 end
