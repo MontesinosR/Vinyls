18fd37c440cd0af61bed6be71bc1a30848660fe7
Loulou Goldfarb <goldfarb.loulou@gmail.com>
2021-08-31 17:55:33 +0200

    Show page mostly finished

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -1,2 +1,5 @@
 class VinylsController < ApplicationController
+  def show
+    @vinyl = Vinyl.find(params[:id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :owner_bookings, through: :vinyls, source: :bookings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  validates :username, presence: true, uniqueness: true
+  # validates :username, presence: true, uniqueness: true
   validates :password, presence: true
   validates :email, presence: true, uniqueness: true
 
