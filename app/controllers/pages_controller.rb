3525fad75b0533f9c1ed73b65f032b9494178c4c
Loulou Goldfarb <goldfarb.loulou@gmail.com>
2021-09-01 16:37:43 +0200

    Can now make a booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,31 +5,31 @@ class PagesController < ApplicationController
 
   end
 
-  def dashboard
-  # bought
-    # pending
-    @bookings_bought_pending = Booking.where(user_id: current_user, status: "Pending")
-    # upcoming
-    @bookings_bought_upcoming = Booking.where(user_id: current_user, status: "Upcoming")
-    # past
-    @bookings_bought_completed = Booking.where(user_id: current_user, status: "Completed")
-    # cancelled
-    @bookings_bought_cancelled = Booking.where(user_id: current_user, status: "Cancelled")
+#   def dashboard
+#   # bought
+#     # pending
+#     @bookings_bought_pending = Booking.where(user_id: current_user, status: "Pending")
+#     # upcoming
+#     @bookings_bought_upcoming = Booking.where(user_id: current_user, status: "Upcoming")
+#     # past
+#     @bookings_bought_completed = Booking.where(user_id: current_user, status: "Completed")
+#     # cancelled
+#     @bookings_bought_cancelled = Booking.where(user_id: current_user, status: "Cancelled")
 
-  # sold
-    # pending
-    @bookings_sold_pending = current_user.owner_bookings.where(status: "Pending")
-    # upcoming
-    @bookings_sold_upcoming = current_user.owner_bookings.where(status: "Upcoming")
-    # past
-    @bookings_sold_completed = current_user.owner_bookings.where(status: "Completed")
-    # cancelled
-    @bookings_sold_cancelled = current_user.owner_bookings.where(status: "Cancelled")
+#   # sold
+#     # pending
+#     @bookings_sold_pending = current_user.owner_bookings.where(status: "Pending")
+#     # upcoming
+#     @bookings_sold_upcoming = current_user.owner_bookings.where(status: "Upcoming")
+#     # past
+#     @bookings_sold_completed = current_user.owner_bookings.where(status: "Completed")
+#     # cancelled
+#     @bookings_sold_cancelled = current_user.owner_bookings.where(status: "Cancelled")
 
-    # owned
-    #owned
-  @vinyls_owned = Vinyl.where(user_id: current_user)
+#     # owned
+#     #owned
+#   @vinyls_owned = Vinyl.where(user_id: current_user)
 
-end
+# end
 
 end
