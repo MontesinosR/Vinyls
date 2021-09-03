4842404c13e6158d0366660ce57e8a72a724256d
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-09-03 10:51:23 +0200

    styling and display of the cards

--- app/controllers/vinyls_controller.rb
+++ app/controllers/vinyls_controller.rb
@@ -33,8 +33,8 @@ class VinylsController < ApplicationController
     @vinyl = Vinyl.new(vinyl_params)
     @vinyl.user = current_user
     if @vinyl.save
-      redirect_to user_bookings_path(current_user, anchor: "owned")
-      flash[:notice] = "Vinyl successfully added to your collection"
+      redirect_to user_bookings_path(current_user)
+     # flash[:notice] = "Vinyl successfully added to your collection"
     else
       render :new
     end
